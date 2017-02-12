0504812b2117bd69d013fd87469236c1d20a34d6
honey164 <himanshu1641997@gmail.com>
2017-02-12 13:12:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,10 @@
 console.log('Loaded!');
 var img = document.getElementById('madi');
-img.onclick =function(){
-    img.style.marginLeft  = '100px';
+var marginLeft = 0;
+function moveRight () {
+marginLeft = marginLeft +1;
+img.style.marginLeft= marginLeft + 'px';
+}
+img.onclick = function () {
+var interval = setInterval(moveRight, 100);
 };
